afe1d8326e7a0b0971ac7675e06c99f3c76dc1cb
Lukhanyo Kalashe <kalashslukhanyo@gmail.com>
2026-03-18 09:51:38 +0200

    End Of Section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,7 @@
 
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
 public class AppUser
@@ -8,8 +10,14 @@ public class AppUser
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
 
+    public string? ImageUrl { get; set; }
+
     public required  byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; } //Add a Paswword Hash 
+
+    //Navigatiion Property
+   
+    public Member Member { get; set; } = null!;
 
 
 
